cda0d75f318114bdc83d0ece748bdf8e8fc655f7
nolooker <121871676+nolooker@users.noreply.github.com>
2024-02-09 16:01:36 +0900

    commit

---
+++ src/main/java/com/folder/app/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.folder.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MemberController {
+    // 회원가입 페이지 출력 요청
+
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(     @RequestParam("memberEmail") String memberEmail
+                       ,    @RequestParam("memberPassword") String memberPassword
+                       ,    @RequestParam("memberName") String memberName ) {
+        System.out.println("MemberController.save");
+        System.out.println(memberEmail + " / " + memberPassword + " / " + memberName);
+        return "index";
+    }
+}
